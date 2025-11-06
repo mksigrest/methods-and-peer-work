8fcf1ddaae0ecc6f96069764ac833ae2570a31a5
mksigrest <mksigrest@gmail.com>
2025-11-06 08:34:31 -0500

    init push

---
+++ methods-and-peer-work/methods-and-peer-work/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int maxsFavoriteNum;
+        int samsFavoriteNum;
+
+        //Initialize
+        maxsFavoriteNum = 8;
+        samsFavoriteNum = 13;
+
+        int Subtract (int int1, int int2) {
+            int value
+        }
+
+        //Start of the program
+        Console.WriteLine("Max's Favorite Number is " + maxsFavoriteNum);
+        Console.WriteLine("Sam's Favorite Number is " + samsFavoriteNum);
+    }
+
+    static int Add (int int1, int int2)
+    {
+        int value;
+        value = int1 + int2;
+        return value;
+    }
+}
